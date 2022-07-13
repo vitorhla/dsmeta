1db654b2492073eda398767690f44074db937215
vitorhla <vitorhenriquealt@gmail.com>
2022-07-12 21:54:43 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
 return(
-  <h1>Teste</h1>
+  <>
+    <h1>Teste</h1>
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  
+  </>
+
 )
 }
 
