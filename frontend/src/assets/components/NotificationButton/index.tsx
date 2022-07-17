65c65059ebab2654a2264974a3bcdc9025acaf59
vitorhla <vitorhenriquealt@gmail.com>
2022-07-16 21:29:57 -0300

    Send notification

--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,13 +1,28 @@
-import icon from '../../img/notification-icon.svg'
-import './styles.css'
+import axios from 'axios';
+import { BASE_URL } from '../../../utils/request';
+import icon from '../../img/notification-icon.svg';
+import './styles.css';
 
 
+type Props = {
+  saleId: number;
+
+}
+
+function handleClick(id: number){
+
+axios(`${BASE_URL}/sales/${id}/notification`)
+.then(response =>{
+
+})
+
+}
 
 
-function NotificationButton() {
+function NotificationButton({saleId} : Props) {
     return (
         
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={()=>handleClick(saleId)}>
           <img src={icon} alt="Notificar" />
         </div>
       
